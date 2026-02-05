8ba9cc8d09602e3384a660181541e601af6ae70e
Priyank Patel <priyank2112006@gmail.com>
2026-02-05 16:30:47 +0530

    navigate place order to order page

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import Title from '../components/Title.jsx';
 import CartTotal from '../components/CartTotal.jsx';
 import { assets } from '../assets/assets.js';
+// import { navigate } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
 
 const PlaceOrder = () => {
-  const [method, setMethod] = React.useState('cod');
+  const [method, setMethod] = useState('cod');
+  const { navigate } = useContext(ShopContext);
 
   return (
     <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:py-14 min-h-[80vh] border-t">
@@ -105,7 +109,10 @@ const PlaceOrder = () => {
             </div>
           </div>
           <div className="w-full text-end mt-8">
-            <button className="bg-black text-white  px-16 py-3 text-sm ">
+            <button
+              onClick={() => navigate('/orders')}
+              className="bg-black text-white  px-16 py-3 text-sm "
+            >
               PLACE ORDER
             </button>
           </div>
