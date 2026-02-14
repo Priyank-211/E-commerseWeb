b764b39bdaa74b70a72897163957d610c0e5a70b
Priyank Patel <priyank2112006@gmail.com>
2026-02-14 11:30:50 +0530

    about page created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import { assets } from '../assets/assets.js';
-import { Link, NavLink } from 'react-router-dom';
-import { useState } from 'react';
-import { useContext } from 'react';
-import { ShopContext } from '../context/ShopContext';
+import { assets } from "../assets/assets.js";
+import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
+import { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
   const [visible, setvisible] = useState(false);
@@ -38,11 +38,13 @@ const Navbar = () => {
           onClick={() => setShowSearch(true)}
         />
         <div className="group relative ">
-          <img
-            className="w-5 cursor-pointer"
-            src={assets.profile_icon}
-            alt=""
-          />
+          <Link to="/login">
+            <img
+              className="w-5 cursor-pointer"
+              src={assets.profile_icon}
+              alt=""
+            />
+          </Link>
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded ">
               <p className="cursor-pointer hover:text-black">My Profile</p>
@@ -67,7 +69,7 @@ const Navbar = () => {
       {/* sidebar menu for small screen */}
       <div
         className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
-          visible ? 'w-full' : 'w-0'
+          visible ? "w-full" : "w-0"
         }`}
       >
         <div className="flex flex-col text-gray-600">
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,69 @@
+import Title from "../components/Title";
 import React from "react";
+import { assets } from "../assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"About"} text2={"Us"} />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img src={assets.about_img} alt="" className="w-full md:max-w-112.5" />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est ipsum
+            vel, doloremque fugiat, iste dolores sit assumenda, aliquid dolorem
+            et excepturi. Illum, cum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
+            sequi. Optio corporis distinctio deleniti nobis harum quo doloremque
+            necessitatibus praesentium delectus quisquam recusandae aut, sequi
+            culpa iste dolorem.
+          </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure eaque
+            sapiente tenetur tempora soluta consequatur illo expedita, corrupti
+            et. Maiores fugiat, optio dolores eligendi omnis, voluptatibus
+            aliquam qui natus dolorem alias aspernatur at est cum.
+          </p>
+        </div>
+      </div>
+      <div className="text-4xl py-4 ">
+        <Title text1={"Why"} text2={"Choose us"} />
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex=col gap-5">
+          <b>Quality Assurance</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+            maiores adipisci accusamus reiciendis odit recusandae, officia velit
+            voluptates.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex=col gap-5">
+          <b>Convenient</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+            maiores adipisci accusamus reiciendis odit recusandae, officia velit
+            voluptates.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex=col gap-5">
+          <b>Exceptional Customer Service</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+            maiores adipisci accusamus reiciendis odit recusandae, officia velit
+            voluptates.
+          </p>
+        </div>
+      </div>
+      <NewsletterBox />
+    </div>
+  );
 };
 
 export default About;
